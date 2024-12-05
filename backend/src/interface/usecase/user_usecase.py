c0b5a37c91c338581dfca2a353b46bbd18ae0017
ekokurniawann <ekokurniwann>
2024-12-05 23:34:02 +0700

    fix: change NotADirectoryError to NotImplementedError in user_usecase.py

--- backend/src/interface/usecase/user_usecase.py
+++ backend/src/interface/usecase/user_usecase.py
@@ -5,5 +5,5 @@ from src.domain.user import User
 class UserUsecase(metaclass=ABCMeta):
   @abstractmethod
   async def create_user(self, user: User) -> User:
-    raise NotADirectoryError
+    raise NotImplementedError
   
